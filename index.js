f431276b03f0f73aaf596aeb616387d8012108b4
Jalani Paul <jalani2727@gmail.com>
2018-07-17 11:20:37 -0400

    added simpler functions for transofrming arrays

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// // PLAIN OL Javascript CODE
+// // PLAIN OL Javascript DOM Programming CODE
 // let hello = document.createElement('h1')
 
 // hello.textContent = "What up"
@@ -15,12 +15,24 @@
 
 const h = React.createElement;
 
+let kebabs = [
+    "Roach Legs (Back-Left Only)",
+    "Cactus Spines",
+    "Nuts"
+];
+// Turn a kebab item into a vitrual DOM element
+// .map will transoform an array it's given and spit back a new one
+let items = kebabs.map((k) => {
+    let vItem = h("li", null, k)
+    return vItem;
+});
 
-// Make the thing 
+// Make the thing (these are virtual DOM elements)
 let title = h("h1", null, "Kebabs");
-let item = h("li", null, "chicken hearts");
-let list = h("ul", null, item);
+// let item = h("li", null, "Roach Legs (Back-Left Only)");
+// let itme2 = h("li", null, "Cactus Spines")
 
+let list = h("ul", null, items);
 
 let main = h("div", null, [
     title, 
