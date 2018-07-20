80cd97cd0316668d8208ec88cd9fe2ea06e56efd
Jalani Paul <jalani2727@gmail.com>
2018-07-20 08:40:11 -0400

    added printmessage button to the box function jsx content

--- smallexercises/src/box.js
+++ smallexercises/src/box.js
@@ -4,11 +4,15 @@ import "./index.css"
    function Box(props) {
         return(
             <div className="box">
+            <button onClick={printMessage} > </button>
                 {props.message}
             </div>
         )
     };
 
+    function printMessage() {
+        console.log("Hello from the box!");
+    }
 
 
 export default Box;
